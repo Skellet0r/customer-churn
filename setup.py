a9ef3529c5ab3d40fa83fe5ed73e56d736d509c0
Edward Amor <edward.amor3@gmail.com>
2020-07-11 23:01:53 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+This setup.py allows for the src directory to be pip installed.
+This means scripts/modules/functions in the src directory will be
+available in the environment such as in jupyter notebooks.
+
+Additional fields are left blank but can be filled in optionally.
+
+To install:
+pip install -e .
+--or--
+pip install -r requirements.txt
+
+To make it even easier we can add the package to the environment.yml
+having a requirements.txt is only a convenience.
+
+https://github.com/conda/conda/blob/54e4a91d0da4d659a67e3097040764d3a2f6aa16/tests/conda_env/support/advanced-pip/environment.yml
+"""
+
+import setuptools
+
+setuptools.setup(
+    name="src",
+    version="",
+    author="",
+    author_email="",
+    description="",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)
